f70221da5ce8f6e36dea8458c1cf41eac6092319
Allandino Manalo <allandinomanalo@gmail.com>
2026-07-12 21:06:01 +0800

    Implement user update functionality in update.php
    Added functionality to update username and password.

--- update.php
+++ update.php
@@ -17,6 +17,35 @@ $datas = mysqli_fetch_assoc($result);
 $username = $datas['username'];
 $password = $datas['password'];
 
+//now for the update mysql using php
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+//first detects if there is a new password input
+
+$newUsername = $_POST['newUsername'];
+$newPassword = $_POST['newPassword'];
+
+//now this is how the magic happens 
+
+$sql = "UPDATE users SET 
+username = '$newUsername',
+password = '$newPassword'
+WHERE id = '$id'";  // we update the datas using update and set where we base the user? using the id sessioned already from the login 
+
+//now execute the mysql 
+
+mysqli_query($cann , $sql);
+
+//to refresh the whole page 
+
+header("Location: update.php");
+exit(); // simply reaload
+
+
+
+
+}
+
 
 ?>
 
@@ -31,6 +60,15 @@ $password = $datas['password'];
     
     <h1>Name: <?php echo ' ' . $username; ?></h1>
     <h1>Password: <?php echo  ' ' . $password; ?></h1>
+
+    <h2>Update your username and password</h2>
+
+    <form  method="post">
+        <input type="text" placeholder="Enter new username" name="newUsername"> <br>
+        <input type="password" placeholder="Enter new password" name="newPassword">
+
+        <button type="submit">Confirm</button>
+    </form>
 
 </body>
 </html>
